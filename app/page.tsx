319d941d87be3e51f542e61860d7fbead2824825
Josef Leventon <josef.leventon@gmail.com>
2024-10-26 17:08:50 -0400

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,208 @@
+"use client";
+
+import { makeSignDoc, StdTx } from "@cosmjs/amino";
+import { getWallet, useConnect, WalletType } from "graz";
 import Image from "next/image";
+import { useState } from "react";
+import {
+  GetResponseType as AuthorizationsGetResponse,
+  SetResponseType as AuthorizationsSetResponse,
+} from "@swiftprotocol/api/routes/notify/auth/types";
+
+const emailRegex = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/;
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [email, setEmail] = useState("");
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  const { connectAsync } = useConnect();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  async function signup() {
+    if (!email) return;
+    const isValidEmail = emailRegex.test(email);
+    if (!isValidEmail) {
+      alert("Invalid email address");
+      return;
+    }
+
+    const key = await connectAsync({
+      chainId: "cosmoshub-4",
+      walletType: WalletType.KEPLR,
+    });
+    console.log(key);
+    const address = key.accounts["cosmoshub-4"].bech32Address;
+
+    const wallet = getWallet(WalletType.KEPLR);
+
+    const authMsg =
+      "I am signing this message to authorize ATOM Accelerator DAO to sign me up for an email newsletter.";
+
+    let signDoc = makeSignDoc(
+      [
+        {
+          type: "sign/MsgSignData",
+          value: {
+            signer: address,
+            data: btoa(authMsg),
+          },
+        },
+      ],
+      {
+        gas: "0",
+        amount: [],
+      },
+      "",
+      "",
+      0,
+      0
+    );
+
+    const sig = await wallet.signAmino("cosmoshub-4", address, signDoc);
+
+    const walletSignature: StdTx = {
+      msg: [
+        {
+          type: "sign/MsgSignData",
+          value: {
+            signer: address,
+            data: btoa(authMsg),
+          },
+        },
+      ],
+      fee: { gas: "0", amount: [] },
+      memo: "",
+      signatures: [sig.signature],
+    };
+
+    const api = process.env.NEXT_PUBLIC_SWIFT_API!;
+    const appId = parseInt(process.env.NEXT_PUBLIC_SWIFT_APP_ID!);
+
+    const authorizationsResponse = await fetch(api + `/notify/auth/get`, {
+      method: "POST",
+      body: JSON.stringify({
+        address: address,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    let authorizations: AuthorizationsGetResponse["authorizations"];
+
+    if (authorizationsResponse.status !== 200) {
+      authorizations = [];
+    } else {
+      const auth: AuthorizationsGetResponse =
+        await authorizationsResponse.json();
+      authorizations = auth.authorizations;
+    }
+
+    const authIndex = authorizations.findIndex((auth) => auth.app === appId);
+    if (authIndex >= 0) {
+      authorizations[authIndex].methods = ["email"];
+    } else {
+      authorizations.push({
+        app: appId,
+        methods: ["email"],
+      });
+    }
+
+    const setAuthorizationsResponse = await fetch(api + `/notify/auth/set`, {
+      method: "POST",
+      body: JSON.stringify({
+        signature: walletSignature,
+        authorizations: authorizations,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (setAuthorizationsResponse.status !== 200) {
+      throw new Error("Failed to set authorizations");
+    } else {
+      setIsSubscribed(true);
+    }
+  }
+  return (
+    <main className="mt-16 max-w-4xl mx-auto">
+      {isSubscribed ? (
+        <div className="bg-blue-700 rounded-xl p-8">
+          <p className="text-white font-semibold text-3xl">
+            Thank you for subscribing!
+          </p>
+          <p className="text-white text-sm mt-2">
+            You should receive your first newsletter soon.
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      ) : (
+        <div className="bg-blue-700 rounded-xl pt-8 px-8 flex flex-row justify-between items-start">
+          <div className="pr-8">
+            <p className="text-white font-semibold text-3xl">
+              Sign up for our newsletter
+            </p>
+            <p className="text-white text-sm mt-2">
+              Receive <b>monthly updates</b> from the ATOM Accelerator DAO with
+              detailed information on your delegations and latest governance
+              happenings.
+            </p>
+            <div className="grid grid-cols-2 gap-4 mt-6">
+              <input
+                className="border-white/25 text-white bg-white/10 rounded-xl border px-4"
+                type="email"
+                placeholder="Your email"
+                value={email}
+                onChange={(e) => setEmail(e.currentTarget.value)}
+              />
+              <button
+                onClick={signup}
+                className="bg-white hover:text-white hover:bg-white/10 border border-transparent hover:border-white/25 rounded-xl px-6 py-4 text-black inline-flex font-medium justify-center items-center flex-row space-x-2"
+              >
+                <Image
+                  src="/keplr.svg"
+                  width={256}
+                  height={256}
+                  className="w-6 h-6"
+                  alt="Keplr"
+                />
+                <p>Sign up with Keplr</p>
+              </button>
+            </div>
+            <span className="text-xs inline-flex space-x-2 mt-6 text-white">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="white"
+                className="size-4 flex-shrink-0"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+                />
+              </svg>
+              <p>
+                Your email address will be encrypted using a temporary key,
+                which will be deleted immediately after encryption. Only{" "}
+                <b>ATOM Accelerator DAO</b> (sw-app-3) will be able to decrypt
+                your information.
+              </p>
+            </span>
+          </div>
+          <div className="flex flex-col justify-between flex-shrink-0">
+            <div></div>
+            <Image
+              width={1080}
+              height={1006}
+              loading="eager"
+              src="/newsletter.png"
+              alt="Newsletter"
+              className="h-64 w-auto"
+            />
+          </div>
+        </div>
+      )}
+    </main>
   );
 }
